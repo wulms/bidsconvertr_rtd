7b0d66f5470fc72c9bfc3a0742b437e04ead4953
Niklas Wulms <niklas.wulms@live.de>
2022-07-18 15:40:07 +0200

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,6 +54,9 @@ templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
+source_suffix = [".rst", ".md"]
+master_doc = "index"
+
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
